d82d52574402e1301ed2cade22bb02336319ca10
mubeenghauri <p176107@nu.edu.pk>
2019-05-08 21:11:48 +0500

    used invoke to call proedures

--- hm.asm
+++ hm.asm
@@ -3,6 +3,14 @@
 include irvine32.inc
 include macros.inc
 INCLUDELIB user32.lib
+;includelib irvine32.lib
+
+BufSize = 80		; defining buffer size
+
+.data
+	buffer BYTE BufSize DUP(?),0,0
+	stdInHandle DWORD ?
+	bytesRead   DWORD ?
 
 .code 
 
@@ -28,7 +36,76 @@ main PROC
     call WaitMsg
     call ClrScr
 
+    call initTextArea
+    call GetInput
+
 exit
 
 main endp
+
+initTextArea PROC 
+
+	mGotoxy 30, 2
+	mWrite "Start Entering Text !!!"
+	mGotoxy 0, 5
+
+	ret
+
+initTextArea ENDP
+
+; GetKeyInput PROC 
+
+; 	; Taking input using readkey
+
+; 	mov ecx, BufSize
+; 	mov ebx, 0
+
+; 	getInput:
+
+; 		;call readkey
+; 		jz nextKey
+
+; 		mov buffer[ebx], al
+; 		call writechar
+; 		call crlf
+
+
+; 		nextKey:
+
+
+; 	loop getInput
+
+; GetKeyInput ENDP
+
+GetInput PROC
+
+	; Get handle to standard input
+	INVOKE GetStdHandle, STD_INPUT_HANDLE
+	mov stdInHandle,eax
+
+	; Wait for user input
+	INVOKE ReadConsole, stdInHandle, ADDR buffer,
+	  BufSize, ADDR bytesRead, 0
+
+	; Display the buffer
+	;mov  esi,OFFSET buffer
+	;mov  ecx,16	; 16 bytes
+	;mov  ebx,TYPE buffer
+	;call DumpMem
+
+
+	;printing buffer
+
+	call crlf 
+	call crlf
+
+	mov edx, offset buffer
+	call writestring
+	call crlf
+	mov eax, bytesRead
+	call writedec
+
+	ret
+GetInput ENDP
+
 end main
